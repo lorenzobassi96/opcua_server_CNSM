dfeda60f0547a8dbd1251ba970707af054f0efbe
lorenzobassi96 <44102822+lorenzobassi96@users.noreply.github.com>
2021-07-10 17:00:40 +0200

    Update get_port_from_json_opcua.py

--- get_json_data/get_port_from_json_opcua.py
+++ get_json_data/get_port_from_json_opcua.py
@@ -2,9 +2,6 @@ import json
 
 with open('opcua_fields.json') as file:
    data = json.load(file)
-   #data["opcua"]["port"]
-   data["opcua"][0]["ip"]
-
 
 valore_porta = data["opcua"][0]["port"]
 print(valore_porta)
