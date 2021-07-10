3e6bc4b7951fa02f89318ed2a6cc10a7c25bab99
lorenzobassi96 <44102822+lorenzobassi96@users.noreply.github.com>
2021-07-10 17:00:53 +0200

    Update get_sensor1_from_json_opcua.py

--- get_json_data/get_sensor1_from_json_opcua.py
+++ get_json_data/get_sensor1_from_json_opcua.py
@@ -3,8 +3,6 @@ import json
 
 with open('opcua_fields.json') as file:
    data = json.load(file)
-   #data["opcua"]["port"]
-   data["opcua"][0]["ip"]
 
 valore_var1 = data["opcua"][1]["objects"][0]["variables"][0]
 print(valore_var1)
