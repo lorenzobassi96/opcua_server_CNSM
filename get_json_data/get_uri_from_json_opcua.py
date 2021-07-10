23c8ce0dd9c5e314b5a1db1674499f681253b9b1
lorenzobassi96 <44102822+lorenzobassi96@users.noreply.github.com>
2021-07-10 17:02:19 +0200

    Update get_uri_from_json_opcua.py

--- get_json_data/get_uri_from_json_opcua.py
+++ get_json_data/get_uri_from_json_opcua.py
@@ -2,8 +2,6 @@ import json
 
 with open('opcua_fields.json') as file:
    data = json.load(file)
-   #data["opcua"]["port"]
-   data["opcua"][0]["ip"]
 
 valore_uri = data["opcua"][0]["uri"]
 print(valore_uri)
