1ef7f26afc508cdb7df2c88eb7cabd7d513526e0
lorenzobassi96 <44102822+lorenzobassi96@users.noreply.github.com>
2021-07-13 08:33:44 +0200

    Create get_all_sensors.py

---
+++ get_json_data/get_all_sensors.py
@@ -0,0 +1,15 @@
+import json
+
+
+with open('opcua_fields.json') as file:
+   data = json.load(file)
+
+
+dimensione_lista = len(data["opcua"][1]["objects"][0]["variables"])
+
+
+i=0
+while (i<dimensione_lista):
+     valore_var = data["opcua"][1]["objects"][0]["variables"][i]
+     print(valore_var)
+     i=i+1
