4737744d244e6b96f9307f94e389b2603b223465
lorenzobassi96 <44102822+lorenzobassi96@users.noreply.github.com>
2021-07-14 17:07:17 +0200

    Update get_object_name_from_json_opcua.py

--- get_json_data/get_object_name_from_json_opcua.py
+++ get_json_data/get_object_name_from_json_opcua.py
@@ -3,7 +3,7 @@ import json
 with open('opcua_fields.json') as file:
    data = json.load(file)
 
-valore_object_name = data["opcua"][1]["objects"][0]["object_name"]
+valore_object_name = data["opcua"][0]["objects"][0]["object_name"]
 print(valore_object_name)
 
 
